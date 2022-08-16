e4cc4b0243f668bb0955f6abc90844a5ecbf9671
Daniil Bobrov <bobdaniil@yandex.ru>
2022-08-16 15:05:20 +0300

    created script for switcher lang

--- website/src/js/secondary.js
+++ website/src/js/secondary.js
@@ -1,1 +1,22 @@
-console.log('Secondary Script');
+const switcher = document.getElementById('lang-switch');
+
+const child = switcher.children;
+
+const sweden = child[0];
+const eng = child[1];
+
+let isShow = false;
+
+switcher.addEventListener('click', (event) => {
+  if (isShow === false) {
+    event.preventDefault();
+    eng.classList.add('show');
+    eng.style.display = 'list-item';
+    isShow = true;
+  }
+  else {
+    eng.classList.remove('show');
+    eng.style.display = 'none';
+    isShow = false;
+  }
+});
