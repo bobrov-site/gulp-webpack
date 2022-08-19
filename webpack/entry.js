b161038885a00d98db3d258fd3378fbbfde4fb57
Daniil Bobrov <bobdaniil@yandex.ru>
2022-08-19 17:07:48 +0300

    finished first page

--- webpack/entry.js
+++ webpack/entry.js
@@ -1,5 +1,4 @@
 module.exports = {
   scripts: './website/src/js/scripts.js',
-  switchToSwe: './website/src/js/switch-to-swe.js',
-  switchToEng: './website/src/js/switch-to-eng.js',
+  switchToSwe: './website/src/js/switch-to-swe.js'
 };
